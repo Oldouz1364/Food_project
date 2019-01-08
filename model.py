7e92735762561e25a99a65114ebf2d24b188a9c7
Oldouz Majidi <35107848+Oldouz1364@users.noreply.github.com>
2019-01-08 09:49:05 +0100

    Update model.py

--- model.py
+++ model.py
@@ -1,1 +1,59 @@
-# Model
+# Convolutional Neural networks Model
+
+%matplotlib inline
+import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+
+## LOAD the mnist data. 
+
+# To speed up training we'll only work on a subset of the data.
+# Note that we reshape the data from 
+#  (nsamples, num_features) = (nsamples, nchannels*rows*cols)
+#    -> (nsamples, nchannels, rows, cols)
+# in order to retain the spatial arrangements of the pixels
+data = np.load('mnist.npz')
+num_classes = 10
+nchannels, rows, cols = 1, 28, 28
+
+x_train = data['X_train'][:1000].astype('float32')
+x_train = x_train.reshape((-1, nchannels, rows, cols))
+targets_train = data['y_train'][:1000].astype('int32')
+
+x_valid = data['X_valid'][:500].astype('float32')
+x_valid = x_valid.reshape((-1, nchannels, rows, cols))
+targets_valid = data['y_valid'][:500].astype('int32')
+
+x_test = data['X_test'][:500].astype('float32')
+x_test = x_test.reshape((-1, nchannels, rows, cols))
+targets_test = data['y_test'][:500].astype('int32')
+
+print("Information on dataset")
+print("x_train", x_train.shape)
+print("targets_train", targets_train.shape)
+print("x_valid", x_valid.shape)
+print("targets_valid", targets_valid.shape)
+print("x_test", x_test.shape)
+print("targets_test", targets_test.shape)
+
+
+## plot a few MNIST examples
+
+idx, dim, classes = 0, 28, 10
+# create empty canvas
+canvas = np.zeros((dim*classes, classes*dim))
+
+# fill with tensors
+for i in range(classes):
+    for j in range(classes):
+        canvas[i*dim:(i+1)*dim, j*dim:(j+1)*dim] = x_train[idx].reshape((dim, dim))
+        idx += 1
+
+# visualize matrix of tensors as gray scale image
+plt.figure(figsize=(6, 6))
+plt.axis('off')
+plt.imshow(canvas, cmap='gray')
+plt.title('MNIST handwritten digits')
+plt.show()
+
+
